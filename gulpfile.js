c2e071a0c62f17ee8e5a0d7a5bd2df1dd88bcf8d
AkmalKurbanov <250296a@gmail.com>
2024-09-28 19:34:07 +0600

    root variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,248 +1,273 @@
-let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
-	fileswatch = 'html,pug,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
-import pkg from 'gulp'
-const {
-	gulp,
-	src,
-	dest,
-	parallel,
-	series,
-	watch
-} = pkg
+let preprocessor = "sass"; // Preprocessor (sass)
 
+import pkg from "gulp";
+const { src, dest, parallel, series, watch } = pkg;
 
+import Fs from "fs";
+import browserSync from "browser-sync";
+import webpackStream from "webpack-stream";
+import webpack from "webpack";
+import TerserPlugin from "terser-webpack-plugin";
+import gulpSass from "gulp-sass";
+import dartSass from "sass";
+import sassglob from "gulp-sass-glob";
+const sass = gulpSass(dartSass);
+import postCss from "gulp-postcss";
+import cssnano from "cssnano";
+import autoprefixer from "autoprefixer";
+import imagemin from "gulp-imagemin";
+import imageminWebp from "imagemin-webp";
+import imageminSvgo from "imagemin-svgo";
+import imageminOptipng from "imagemin-optipng";
+import changed from "gulp-changed";
+import concat from "gulp-concat";
+import rsync from "gulp-rsync";
+import { deleteAsync } from "del";
+import pug from "gulp-pug";
+import ttf2woff2 from "gulp-ttf2woff2";
+import plumber from "gulp-plumber"; // Подключаем gulp-plumber для обработки ошибок
 
-import Fs from 'fs'
+// Обработчик ошибок
+function handleErrors(task) {
+  return plumber({
+    errorHandler: function (err) {
+      console.error(`Error in ${task} task:`, err.toString());
+      this.emit("end"); // Не прерываем задачу при ошибке
+    },
+  });
+}
 
-import browserSync from 'browser-sync'
-import bssi from 'browsersync-ssi'
-import ssi from 'ssi'
-import webpackStream from 'webpack-stream'
-import webpack from 'webpack'
-import TerserPlugin from 'terser-webpack-plugin'
-import gulpSass from 'gulp-sass'
-import dartSass from 'sass'
-import sassglob from 'gulp-sass-glob'
-const sass = gulpSass(dartSass)
-import less from 'gulp-less'
-import lessglob from 'gulp-less-glob'
-import styl from 'gulp-stylus'
-import stylglob from 'gulp-noop'
-import postCss from 'gulp-postcss'
-import cssnano from 'cssnano'
-import autoprefixer from 'autoprefixer'
-import imagemin from 'imagemin'
-import imageminWebp from 'imagemin-webp'
-import changed from 'gulp-changed'
-import concat from 'gulp-concat'
-import rsync from 'gulp-rsync'
-import pug from 'gulp-pug'
+// Удаление оригинальных изображений
+function cleanOriginalImages() {
+  return deleteAsync(["app/images/dist/**/*.{jpg,jpeg,png}"], { force: true });
+}
 
-
-
-
-
-import {
-	deleteAsync
-} from 'del'
-
-
+// Настройка BrowserSync
 function browsersync() {
-	browserSync.init({
-		server: {
-			baseDir: 'app/',
-			middleware: bssi({
-				baseDir: 'app/',
-				ext: '.html'
-			})
-		},
-		ghostMode: {
-			clicks: false
-		},
-		notify: false,
-		online: true,
-		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
-	})
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+    ghostMode: {
+      clicks: false,
+    },
+    notify: false,
+    online: true,
+  });
 }
 
-
-
+// Обработка скриптов с ошибками
 function scripts() {
-	return src([
-		'app/js/*.js',
-		'!app/js/*.min.js'
-	])
-		.pipe(webpackStream({
-			mode: 'production',
-			performance: {
-				hints: false
-			},
-			plugins: [
-				new webpack.ProvidePlugin({
-					$: 'jquery',
-					jQuery: 'jquery',
-					'window.jQuery': 'jquery'
-				}), // jQuery (npm i jquery)
-			],
-			module: {
-				rules: [{
-					test: /\.m?js$/,
-					exclude: /(node_modules)/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env'],
-							plugins: ['babel-plugin-root-import']
-						}
-					}
-				}]
-			},
-			optimization: {
-				minimize: true,
-				minimizer: [
-					new TerserPlugin({
-						terserOptions: {
-							format: {
-								comments: false
-							}
-						},
-						extractComments: false
-					})
-				]
-			},
-		}, webpack)).on('error', (err) => {
-			this.emit('end')
-		})
-		.pipe(concat('app.min.js'))
-		.pipe(dest('app/js'))
-		.pipe(browserSync.stream())
+  return src(["app/js/*.js", "!app/js/*.min.js"])
+    .pipe(handleErrors("scripts"))
+    .pipe(
+      webpackStream(
+        {
+          mode: "production",
+          performance: {
+            hints: false,
+          },
+          plugins: [
+            new webpack.ProvidePlugin({
+              $: "jquery",
+              jQuery: "jquery",
+              "window.jQuery": "jquery",
+            }),
+          ],
+          module: {
+            rules: [
+              {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ["@babel/preset-env"],
+                    plugins: ["babel-plugin-root-import"],
+                  },
+                },
+              },
+            ],
+          },
+          optimization: {
+            minimize: true,
+            minimizer: [
+              new TerserPlugin({
+                terserOptions: {
+                  format: {
+                    comments: false,
+                  },
+                },
+                extractComments: false,
+              }),
+            ],
+          },
+        },
+        webpack
+      )
+    )
+    .pipe(concat("app.min.js"))
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
+// Обработка стилей без кэширования и зависимостей
 function styles() {
-	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
-		.pipe(eval(`${preprocessor}glob`)())
-		.pipe(eval(preprocessor)({
-			'include css': true
-		}))
-		.pipe(postCss([
-			autoprefixer({
-				grid: 'autoplace'
-			}),
-			cssnano({
-				preset: ['default', {
-					discardComments: {
-						removeAll: true
-					}
-				}]
-			})
-		]))
-		.pipe(concat('app.min.css'))
-		.pipe(dest('app/css'))
-		.pipe(browserSync.stream())
+  return src([
+    `app/styles/${preprocessor}/*.*`,
+    `!app/styles/${preprocessor}/_*.*`,
+  ])
+    .pipe(handleErrors("styles")) // Добавляем обработку ошибок
+    .pipe(sassglob()) // Поддержка глобальных импортов
+    .pipe(
+      sass
+        .sync({
+          outputStyle: "compressed",
+        })
+        .on("error", sass.logError)
+    )
+    .pipe(
+      postCss([
+        autoprefixer({
+          grid: "autoplace",
+        }),
+        cssnano({
+          preset: ["default", { discardComments: { removeAll: true } }],
+        }),
+      ])
+    )
+    .pipe(concat("app.min.css"))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
+// Обработка изображений с ошибками
 async function images() {
+  await src("app/images/src/**/*.{jpg,png,svg,webp,ico}")
+    .pipe(handleErrors("images"))
+    .pipe(changed("app/images/dist"))
+    .pipe(
+      imagemin([
+        imageminWebp({ quality: 80 }),
+        imageminSvgo(),
+        imageminOptipng(),
+      ])
+    )
+    .pipe(dest("app/images/dist"));
 
-	await imagemin(['app/images/src/**/*.{jpg,png,svg,webp,ico}'], {
-		destination: 'app/images/dist',
-		plugins: [
-			imageminWebp({ quality: 80 })
-		]
-	})
-	browserSync.stream()
+  await cleanOriginalImages();
+
+  browserSync.reload();
 }
 
-function buildcopy() {
-	return src([
-		'{app/js,app/css}/*.min.*',
-		'app/images/**/*.*',
-		'!app/images/src/**/*',
-		'app/fonts/**/*',
-		'app/**/*.html'
-	], {
-		base: 'app/'
-	})
-		.pipe(dest('dist'))
-}
-
+// Компиляция Pug в HTML с ошибками
 function pug2html() {
-	let dataFromFile = JSON.parse(Fs.readFileSync('app/pug/includes/data.json'));
-	return src('app/pug/*.pug')
-		.pipe(changed('app', { extension: ['.html'] }))
-		
-		.pipe(
-			pug({
-				doctype: 'html',
-				pretty: true,
-				locals: dataFromFile || {}
-			})
-		)
-		.pipe(dest('app'))
-		.pipe(browserSync.stream());
-};
+  let dataFromFile;
 
+  try {
+    dataFromFile = JSON.parse(Fs.readFileSync("app/pug/includes/data.json"));
+  } catch (error) {
+    console.error("Ошибка чтения JSON:", error);
+    dataFromFile = {};
+  }
 
-async function buildhtml() {
-	let includes = new ssi('app/', 'dist/', '/**/*.html')
-	includes.compile()
-	await deleteAsync('dist/parts', {
-		force: true
-	})
+  return src("app/pug/**/*.pug")
+    .pipe(handleErrors("pug2html"))
+    .pipe(changed("app", { extension: ".html" }))
+    .pipe(
+      pug({
+        doctype: "html",
+        pretty: true,
+        locals: dataFromFile || {},
+      })
+    )
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
 }
 
+// Конвертация TTF файлов в WOFF2 с ошибками
+function fonts() {
+  return src("app/fonts/*.ttf")
+    .pipe(handleErrors("fonts"))
+    .pipe(changed("dist/fonts", { extension: ".woff2" }))
+    .pipe(ttf2woff2())
+    .pipe(dest("dist/fonts"))
+    .on("end", async () => {
+      await deleteAsync("dist/fonts/*.ttf", { force: true });
+    });
+}
+
+// Очистка папки dist с ошибками
 async function cleandist() {
-	await deleteAsync(['dist/**/*', 'app/**/*.html'], {
-		force: true
-	})
+  await deleteAsync(["dist/**/*", "app/**/*.html"], { force: true });
 }
 
+// Копирование файлов для сборки с ошибками
+function buildcopy() {
+  return src(
+    [
+      "{app/js,app/css}/*.min.*",
+      "app/images/**/*.*",
+      "!app/images/src/**/*",
+      "app/fonts/**/*",
+      "app/**/*.html",
+    ],
+    { base: "app/" }
+  )
+    .pipe(handleErrors("buildcopy"))
+    .pipe(dest("dist"));
+}
+
+// Деплой на сервер
 function deploy() {
-	return src('dist/')
-		.pipe(rsync({
-			root: 'dist/',
-			hostname: 'username@yousite.com',
-			destination: 'yousite/public_html/',
-			// clean: true, // Mirror copy with file deletion
-			include: [ /* '*.htaccess' */], // Included files to deploy,
-			exclude: ['**/Thumbs.db', '**/*.DS_Store'],
-			recursive: true,
-			archive: true,
-			silent: false,
-			compress: true
-		}))
+  return src("dist/").pipe(
+    rsync({
+      root: "dist/",
+      hostname: "username@yousite.com",
+      destination: "yousite/public_html/",
+      recursive: true,
+      archive: true,
+      silent: false,
+      compress: true,
+    })
+  );
 }
 
+// Наблюдение за изменениями
 function startwatch() {
-	watch(`app/styles/${preprocessor}/**/*`, {
-		usePolling: true
-	}, styles)
-	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], {
-		usePolling: true
-	}, scripts)
-	watch('app/images/src/**/*', {
-		usePolling: true
-	}, images)
-
-
-	watch('app/pug/**/*', {
-		usePolling: true
-	}, pug2html)
-
-	
-	watch(`app/**/*.{${fileswatch}}`, {
-		usePolling: true
-	}).on('change', browserSync.reload)
+  watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles);
+  watch(
+    ["app/js/**/*.js", "!app/js/**/*.min.js"],
+    { usePolling: true },
+    scripts
+  );
+  watch(
+    "app/images/src/**/*.{jpg,png,svg,webp,ico}",
+    { usePolling: true },
+    images
+  );
+  watch("app/pug/**/*.pug", { usePolling: true }, pug2html);
+  watch("app/fonts/*.ttf", { usePolling: true }, fonts);
+  watch("app/**/*.{html,htm,txt,json,md,woff2}", { usePolling: true }).on(
+    "change",
+    browserSync.reload
+  );
 }
 
-
-export {
-	scripts,
-	styles,
-	images,
-	deploy
-}
-export let assets = series(scripts, styles, images)
-export let build = series(cleandist, pug2html, buildhtml, scripts, styles, images, buildcopy)
-
-export default series(scripts, styles, pug2html, images, parallel(browsersync, startwatch))
-
+// Экспорт задач
+export { scripts, styles, images, fonts, deploy };
+export let assets = series(scripts, styles, images, fonts);
+export let build = series(
+  cleandist,
+  pug2html,
+  parallel(styles, scripts, images, fonts),
+  buildcopy
+);
+export default series(
+  pug2html,
+  scripts,
+  styles,
+  images,
+  fonts,
+  parallel(browsersync, startwatch)
+);
